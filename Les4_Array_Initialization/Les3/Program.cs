3ea46379a1aaf9805c7fd5918b25afbbb464da2c
Xelagro <70694180+Xelagro@users.noreply.github.com>
2021-11-08 11:20:19 +0100

    Les 4 Array Initialization

---
+++ Les4_Array_Initialization/Les3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Les3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Room room1 = new Room(2,2);
+        }
+    }
+
+    // Tile Class.
+
+    class Tile
+    {
+
+    }
+
+    // Room class.
+
+    class Room
+    {
+        Tile[][] tileMap;
+
+        public Room( int width, int height)
+        {
+            tileMap = new Tile [height][];
+            for (int i = 0; i < tileMap.Length; i++)
+            {
+                tileMap[i] = new Tile[width];
+                for (int K = 0; K < tileMap[i].Length; K++)
+                {
+                    tileMap[i][K] = new Tile();
+                }
+            }
+        }
+    }
+}
