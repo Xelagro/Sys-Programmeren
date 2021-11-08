46e06c901a9fe8f48c340e151ebcafcbb47d9068
Xelagro <70694180+Xelagro@users.noreply.github.com>
2021-11-08 11:12:28 +0100

    Les 6

---
+++ Les6_Inheritance/SYS_Les6_Inheritance/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace SYS_Les6_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    // Hier begint de code
+
+    abstract class PickUps
+    {
+        public virtual void onPickUp()
+        {
+            Console.WriteLine("You pick up the thing");
+        }
+
+        public void HitByMouse(int x, int y)
+        {
+            int ItemX = 10;
+            int ItemY = 30;
+
+            if (x == ItemX && y == ItemY)
+            {
+                onPickUp();
+            }
+        }
+    }
+    abstract class Equipment : PickUps
+    {
+        public virtual void Equip()
+        {
+
+        }
+    }
+
+        // Pickup van armor
+
+    class Armor : Equipment
+    {
+        public override void Equip()
+        {
+            base.Equip();
+            Console.WriteLine("Dit is een helm");
+        }
+    }
+
+        // Pickup vam wapens
+
+    class Weapon : Equipment
+    {
+        public override void Equip()
+        {
+            base.Equip();
+            Console.WriteLine("Dit is een zwaard");
+        }
+    }
+
+        // Pickup heal potion
+
+    class HealingPotion : PickUps
+    {
+        public override void onPickUp()
+        {
+            base.onPickUp();
+            Console.WriteLine("Je hebt nu een HealthPotion in je inv");
+        }
+    }
+        
+        // Pickup Mana Potion.
+
+    class ManaPotion : PickUps
+    {
+        public override void onPickUp()
+        {
+            base.onPickUp();
+            Console.WriteLine("Je hebt nu een ManaPotion in je inv");
+        }
+    }
+}
