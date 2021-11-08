514d5bc52b3e0e7de76875320666da1ef6377254
Xelagro <70694180+Xelagro@users.noreply.github.com>
2021-11-08 11:26:09 +0100

    Les 2/3 rectangle Maken

---
+++ Oefening Rectangle/Les2 Basics(les3)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Les2_Basics_les3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Rectangle rechthoek = new Rectangle(100, 100, 50, 60);
+            Rectangle vierkant = new Rectangle(50, 50, 50);
+            Rectangle vierkantBeweeg = new Rectangle(50);
+            Rectangle rechthoekBeweeg = new Rectangle(20, 30);
+
+            vierkantBeweeg.MoveTo(50, 50);
+            rechthoekBeweeg.MoveTo(100, 100);
+        }
+    }
+}
---
+++ Oefening Rectangle/Les2 Basics(les3)/Rectangle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Les2_Basics_les3_
+{
+    class Rectangle
+    {
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+
+        public Rectangle(int x, int y, int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.height = height;
+        }
+        public Rectangle(int x, int y, int size)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = size;
+            this.height = size;
+        }
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public Rectangle(int size)//viekant
+        {
+            
+        }
+
+        // 2de gedeelte van de opdracht.
+
+        public void MoveTo(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public bool IsSquare()
+        {
+            bool rectangleVierkant;
+
+            if(this.x == this.y)
+            {
+                rectangleVierkant = true;
+            }
+            else
+            {
+                rectangleVierkant = false;
+            }
+            return rectangleVierkant;
+        }
+
+        public void Set(int x, int y, int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
